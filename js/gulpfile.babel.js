5fca12b8cfc76b5b1d07c18eadcfd390041ada51
Fabien Bourrel <fabien@bourrel.me>
2017-01-18 10:15:14 +0100

    First test with a tutorial, just to see how it works

---
+++ js/gulpfile.babel.js
@@ -0,0 +1,64 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import del from 'del';
+import eslint from 'gulp-eslint';
+import flow from 'gulp-flowtype';
+import mocha from 'gulp-mocha';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
+
+const paths = {
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.jsx',
+  gulpFile: 'gulpfile.babel.js',
+  clientBundle: 'dist/client-bundle.js?(.map)',
+  webpackFile: 'webpack.config.babel.js',
+  libDir: 'lib',
+  distDir: 'dist',
+  allLibTests: 'lib/test/**/*.js',
+};
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+    paths.webpackFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(flow({ abort: true }))
+);
+
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
+
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir))
+);
+
+gulp.task('main', ['test'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir)),
+);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('test', ['build'], () =>
+  gulp.src(paths.allLibTests)
+    .pipe(mocha())
+);
+
+gulp.task('default', ['watch', 'main']);
---
+++ js/src/server/index.js
@@ -0,0 +1,8 @@
+// @flow
+/* eslint-disable no-console */
+
+import Dog from '../shared/dog';
+
+const toby = new Dog('Toby');
+
+console.log(toby.bark());
