0b8cb8c5a6d6221ba4e80ad4d8801c6505272c6b
Benoît Latinier <benoit@latinier.fr>
2017-04-04 10:34:58 +0200

    add: somme bootstrap to begin

--- client/main/container.jsx
+++ client/main/container.jsx
@@ -1,15 +1,30 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
+
+// http://stackoverflow.com/a/36933127/1450754
 
 const Main = ({ children }) => (
-  <div>
-    <header>
-      <Link to="/home">Home</Link>
-      <Link to="/statistic">Statistics</Link>
-      <Link to="/login">LogIn</Link>
-      <Link to="/register">SignIn</Link>
-    </header>
+  <div className="container-fluid">
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/home">OnEstPasDesPigeons</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={2}>
+          <Link to="/statistic">Statistics</Link>
+        </NavItem>
+        <NavItem eventKey={3}>
+          <Link to="/login">LogIn</Link>
+        </NavItem>
+        <NavItem eventKey={4}>
+          <Link to="/register">SignIn</Link>
+        </NavItem>
+      </Nav>
+    </Navbar>
     <section>
       <div>
         {children}
