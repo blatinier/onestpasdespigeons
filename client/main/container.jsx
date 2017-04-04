f23279d79be4d77bd6007f8c755ab497939a03a5
Benoît Latinier <benoit@latinier.fr>
2017-04-04 18:32:03 +0200

    add: react-router-bs

--- client/main/container.jsx
+++ client/main/container.jsx
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-
-// http://stackoverflow.com/a/36933127/1450754
 
 const Main = ({ children }) => (
   <div className="container-fluid">
@@ -13,16 +12,16 @@ const Main = ({ children }) => (
           <Link to="/home">OnEstPasDesPigeons</Link>
         </Navbar.Brand>
       </Navbar.Header>
-      <Nav>
-        <NavItem eventKey={2}>
-          <Link to="/statistic">Statistics</Link>
-        </NavItem>
-        <NavItem eventKey={3}>
-          <Link to="/login">LogIn</Link>
-        </NavItem>
-        <NavItem eventKey={4}>
-          <Link to="/register">SignIn</Link>
-        </NavItem>
+      <Nav className="pull-right">
+        <LinkContainer to="/statistic">
+          <NavItem eventKey={2}>Statistics</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/login">
+          <NavItem eventKey={3}>LogIn</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/register">
+          <NavItem eventKey={4}>SignIn</NavItem>
+        </LinkContainer>
       </Nav>
     </Navbar>
     <section>
