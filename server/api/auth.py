f5656b83b83e1025a332738d8bffc01d822b4aa0
Benoît Latinier <benoit@latinier.fr>
2017-04-08 14:43:13 +0200

    add: Some routes for signin/up

--- server/api/auth.py
+++ server/api/auth.py
@@ -1,9 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 auth_bp = Blueprint('api/auth', __name__)
+
 
 @auth_bp.route('/is_token_valid', methods=['POST'])
 def token_valid():
     print('IS TOKEN VALID')
     return {}
+
+
+@auth_bp.route('/create_user', methods=['POST'])
+def create_user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password_confirm = request.form.get('password_confirm')
+    if password == password_confirm:
+        password_hash = User.hash_password(password)
+        user = User(email=email, password=password_hash)
+        user.save()
+        return jsonify({'token': user.get_new_token()})
+    return {}
+
+
+@auth_bp.route('/get_token', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.log_in(email, password)
+    return jsonify({'token': user.get_new_token()})
--- server/models/user.py
+++ server/models/user.py
@@ -1,8 +1,9 @@
+import hashlib
+import uuid
+
 from mongoengine import Document, fields
 
 
-# TODO SSO (fb, google au moins, cf jarr)
-# TODO field validation
 class User(Document):
     meta = {
         'collection': 'users',
@@ -15,3 +16,19 @@ class User(Document):
     name = fields.StringField(required=True)
     email = fields.StringField(required=True)
     role = fields.StringField(required=True)
+    password = fields.StringField(required=True)
+    session_token = fields.StringField()
+
+    @staticmethod
+    def hash_password(pwd):
+        return hashlib.sha1(pwd).hexdigest()
+
+    @staticmethod
+    def log_in(email, pwd):
+        return User.objects.get(email=email,
+                                password=User.hash_password(pwd))
+
+    def get_new_token(self):
+        self.session_token = str(uuid.uuid4())
+        self.save()
+        return self.session_token
