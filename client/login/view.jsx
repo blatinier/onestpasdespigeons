43c39888b057e9fc96259ca0bbd2d68ce11ac73f
Benoît Latinier <benoit@latinier.fr>
2017-04-08 14:43:00 +0200

    Add login form

--- client/login/view.jsx
+++ client/login/view.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { FormGroup, ControlLabel, FormControl,
+         HelpBlock, Button } from 'react-bootstrap';
+
+const FieldGroup = ({ id, label, help, ...props }) => (
+  <FormGroup controlId={id}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl {...props} />
+    {help && <HelpBlock>{help}</HelpBlock>}
+  </FormGroup>
+);
+
+FieldGroup.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  help: PropTypes.string
+};
 
 const LoginView = () => (
-  <h1>
-    Login
-  </h1>
+  <div>
+    <h1>
+      Login
+    </h1>
+    <form>
+      <FieldGroup
+        id="formControlsEmail"
+        type="email"
+        label="Email address"
+        placeholder="Enter email"
+      />
+      <FieldGroup
+        id="formControlsPassword"
+        label="Password"
+        type="password"
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </div>
 );
 
 export default LoginView;
