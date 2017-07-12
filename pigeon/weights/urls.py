ff4a9381ec55aae50dbd1a656c008d576f5baa62
Benoît Latinier <benoit@latinier.fr>
2017-07-12 22:44:15 +0200

    add: major pages architecture

--- pigeon/weights/urls.py
+++ pigeon/weights/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^overview$', views.overview, name='overview'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^account$', views.user_account, name='user_account'),
+    url(r'^contribute$', views.contribute, name='contribute'),
+    url(r'^my_measures$', views.my_measures, name='my_measures'),
+    url(r'^list_measures$', views.list_measures, name='list_measures'),
+    url(r'^password_reset$', views.password_reset, name='password_reset'),
+    url(r'^register$', views.register, name='register'),
 ]
--- pigeon/weights/views.py
+++ pigeon/weights/views.py
@@ -1,4 +1,103 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
 
 def home(request):
+    """
+    Pretty home page.
+    """
     return render(request, 'weights/home.html', {})
+
+
+def about(request):
+    """
+    Everything about licensing/opensource/OpenFoodFacts...
+    """
+    # TODO #20
+    return render(request, 'weights/about.html', {})
+
+
+def register(request):
+    """
+    Register page.
+    """
+    # TODO #1
+    return render(request, 'weights/register.html', {})
+
+
+def password_reset(request):
+    """
+    Reset password page.
+    """
+    # TODO #1
+    return render(request, 'weights/password_reset.html', {})
+
+
+def logout(request):
+    """
+    Logout page.
+    """
+    logout(request)
+    return redirect("/")
+
+
+def login(request):
+    """
+    Login page.
+    """
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        redirect('/overview')
+    else:
+        # TODO #1 Return an 'invalid login / register' error message.
+        pass
+    return render(request, 'weights/login.html', {})
+
+
+@login_required
+def list_measures(request):
+    """
+    List of all measures with all possible manipulation
+    we can imagine.
+    """
+    # TODO
+    return render(request, 'weights/list_measures.html', {})
+
+
+def overview(request):
+    """
+    Some global statistics with nice graphs.
+    """
+    # TODO #19
+    return render(request, 'weights/overview.html', {})
+
+
+@login_required
+def user_account(request):
+    """
+    Everything about user managing his account.
+    """
+    # TODO
+    return render(request, 'weights/user_account.html', {})
+
+
+def contribute(request):
+    """
+    Page to explain how to crontibute.
+    (e.g issu github, add measures, add products, ideas, ...)
+    """
+    # TODO
+    return render(request, 'weights/contribute.html', {})
+
+
+@login_required
+def my_measures(request):
+    """
+    Page to add your own measurements.
+    """
+    # TODO
+    return render(request, 'weights/my_measures.html', {})
