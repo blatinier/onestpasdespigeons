d79276662022ab208d45a6c83c714a9c0e973c0f
Benoît Latinier <benoit@latinier.fr>
2017-07-14 20:04:46 +0200

    clean settings.py

--- pigeon/pigeon/settings.py
+++ pigeon/pigeon/settings.py
@@ -19,13 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(2eq%$3i&o6su-xizp#(rfswbe&mj7gcx40ht1asuj1qfl8$gp'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['workspace.pigeon.latinier.fr']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -106,27 +103,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-
 # Login
-
 LOGIN_URL = '/login'
 
 try:
