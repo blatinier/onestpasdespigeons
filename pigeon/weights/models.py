abe43b080171519fbab83fb9c12d9bc081ee4e1e
Benoît Latinier <benoit@latinier.fr>
2017-07-23 20:50:24 +0200

    fix post save hook

--- pigeon/weights/models.py
+++ pigeon/weights/models.py
@@ -20,6 +20,11 @@ def create_user_profile(sender, instance, created, **kwargs):
         PigeonUser.objects.create(user=instance)
 
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.pigeonuser.save()
+
+
 class Product(models.Model):
     """
     https://world.openfoodfacts.org/data/data-fields.txt
