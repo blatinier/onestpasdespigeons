1d582e13397001a86de63297b0f606bfb6825cc1
Benoît Latinier <benoit@latinier.fr>
2017-07-24 21:43:34 +0200

    fix some pep8

--- pigeon/weights/urls.py
+++ pigeon/weights/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
 from . import views
 
-# TODO auth views https://docs.djangoproject.com/en/1.11/topics/auth/default/#using-the-views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^overview$', views.overview, name='overview'),
@@ -14,12 +12,4 @@ urlpatterns = [
 
     url(r'^register/$', views.register, name='register'),
     url('^', include('django.contrib.auth.urls')),
-    #url(r'^password_change/$', auth_views.PasswordChangeView.as_view()),
-    #url(r'^password_change/done/$', auth_views.PasswordChangeDoneView.as_view()),
-    #url(r'^password_reset/$', auth_views.PasswordResetView.as_view()),
-    #url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view()),
-    #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-),+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view()),
-    #url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view()),
-
-    #url(r'^password_reset$', views.password_reset, name='password_reset'),
 ]
--- pigeon/weights/views.py
+++ pigeon/weights/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from weights.forms import UserForm, ProfileForm
