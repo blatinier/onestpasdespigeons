2634fc240f5513f598950cf36f4911fa9de90501
Benoît Latinier <benoit@latinier.fr>
2017-08-21 00:33:00 +0200

    add: sso login (fb, google, github, twitter) (closes #52)

--- pigeon/pigeon/settings.py
+++ pigeon/pigeon/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django_select2',
     'django_filters',
     'bootstrap_pagination',
+    'social_django',
     'weights',
     'autotranslate',
 ]
@@ -83,6 +84,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -104,6 +107,76 @@ DATABASES = {
         'POST': '',
     }
 }
+
+AUTHENTICATION_BACKENDS = (
+    # http://python-social-auth.readthedocs.io/en/latest/backends/google.html#google-oauth2
+    'social_core.backends.google.GoogleOAuth2',
+    # http://python-social-auth.readthedocs.io/en/latest/backends/facebook.html#oauth2
+    'social_core.backends.facebook.FacebookOAuth2',
+    # http://python-social-auth.readthedocs.io/en/latest/backends/twitter.html
+    'social_core.backends.twitter.TwitterOAuth',
+    # http://python-social-auth.readthedocs.io/en/latest/backends/github.html
+    'social_core.backends.github.GithubOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social_core.pipeline.social_auth.social_details',
+
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social_core.pipeline.social_auth.social_uid',
+
+    # Verifies that the current auth process is valid within the current
+    # project, this is where emails and domains whitelists are applied (if
+    # defined).
+    'social_core.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social_core.pipeline.social_auth.social_user',
+
+    # Make up a username for this person, appends a random string at the end if
+    # there's any collision.
+    'social_core.pipeline.user.get_username',
+
+    # Send a validation email to the user to verify its email address.
+    # Disabled by default.
+    # 'social_core.pipeline.mail.mail_validation',
+
+    # Associates the current social details with another user account with
+    # a similar email address. Disabled by default.
+    # 'social_core.pipeline.social_auth.associate_by_email',
+
+    # Create a user account if we haven't found one yet.
+    'social_core.pipeline.user.create_user',
+
+    # Create the record that associates the social account with the user.
+    'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social_core.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
+    'social_core.pipeline.user.user_details',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
+
+SOCIAL_AUTH_GITHUB_KEY = ''
+SOCIAL_AUTH_GITHUB_SECRET = ''
+
+SOCIAL_AUTH_TWITTER_KEY = ''
+SOCIAL_AUTH_TWITTER_SECRET = ''
+
+SOCIAL_AUTH_FACEBOOK_KEY = ''
+SOCIAL_AUTH_FACEBOOK_SECRET = ''
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -160,6 +233,9 @@ LOGOUT_REDIRECT_URL = '/'
 SELECT2_JS = '/static/js/select2.min.js'
 SELECT2_CSS = '/static/css/select2.min.css'
 SELECT2_I18N_PATH = '/static/js/select2-i18n'
+
+# Social
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 try:
     from pigeon.local_settings import *
--- pigeon/weights/urls.py
+++ pigeon/weights/urls.py
@@ -2,12 +2,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -32,4 +32,5 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url('^', include('django.contrib.auth.urls')),
     url(r'^select2/', include('django_select2.urls')),
+    url(r'^social/', include('social_django.urls', namespace='social')),
 ]
